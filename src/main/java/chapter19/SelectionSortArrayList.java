b43812ce123f66b490ee21d3b16938c4bde39797
Mohammad Khan <37840365+arabianhorse16@users.noreply.github.com>
2019-11-12 03:09:37 -0500

    Finished commenting to all programs in packages chapter18 and chapter19

---
+++ src/main/java/chapter19/SelectionSortArrayList.java
@@ -0,0 +1,59 @@
+package chapter19;
+
+import java.util.ArrayList;
+
+/**
+ * @author Mohammad Khan
+ * This class serves as a HW exercise for chapter 19 for my CSC 202 class.
+ * It functions to take an unordered array of integers and orders them from 
+ * smallest to largest. The correct order is then provided to the user.
+ */
+public class SelectionSortArrayList {
+    
+   /**
+    * Main method for program execution.
+    */
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>(); // Creates an ArrayList object.
+        
+        // Adds four integer values to the array list.
+        list.add(new Integer(8));
+        list.add(new Integer(9));
+        list.add(new Integer(4));
+        list.add(new Integer(5));
+        
+        SelectionSortArrayList.<Integer>selectionSort(list); // Calls on a method to sort the integers from smallets to largest.
+        
+        System.out.println(list); // Prints the ordered array.
+    }
+    
+    public static <E extends Comparable<E>> void selectionSort(ArrayList<E> list) {
+        // Variables to keep track of the current
+        // smallest value in the array list.
+        E currentMin;
+        int currentMinIndex;
+        
+        // For loop that runs until all integers
+        // are sorted.
+        for(int i = 0; i < list.size() - 1; i++) {
+            currentMin = list.get(i); // Sets the current smallest value to what index i is.
+            currentMinIndex = i; // Sets index i as the smallest index.
+            
+            // For loop that runs until the current index is given the
+            // proper integer value in the array list of smallest to largest.
+            for(int j = i + 1; j < list.size(); j++) {
+                if(currentMin.compareTo(list.get(j)) > 0) {
+                    currentMin = list.get(j); // Sets the current smallest value to what index j is.
+                    currentMinIndex = j; // Sets index j as the smallest index.
+                }
+            }
+            
+            // If statement that resets the index and minimum 
+            // integer for when the outer for loop runs again.
+            if(list.get(currentMinIndex) != list.get(i)) {
+                list.set(currentMinIndex, list.get(i));
+                list.set(i, currentMin);   
+            } 
+        }
+    }
+}
